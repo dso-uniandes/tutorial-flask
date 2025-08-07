4d117898a48732fe60853c9bdc44cd398859876e
Daniel Serna Osorio <d.sernao@uniandes.edu.co>
2025-08-07 17:20:24 -0500

    Parte 2: Implementación de las clases

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,5 +1,16 @@
 from flaskr import create_app
+from .modelos import db, Cancion
 
 app = create_app("default")
 app_context = app.app_context()
 app_context.push()
+
+db.init_app(app)
+db.create_all()  
+
+# Prueba
+with app.app_context():
+    cancion = Cancion(titulo="Despacito", minutos=3, segundos=47, interprete="Luis Fonsi")
+    db.session.add(cancion)
+    db.session.commit()
+    print(Cancion.query.all())  
