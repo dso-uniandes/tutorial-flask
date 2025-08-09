a63339d004e1566d8305cb2cd7f17734c440b6a4
Daniel Serna Osorio <d.sernao@uniandes.edu.co>
2025-08-08 21:24:18 -0500

    Parte 6: Pruebas a la aplicación

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,24 +1,20 @@
 from flaskr import create_app
-from .modelos import db, Cancion, Album
-from .modelos import AlbumSchema
 from flask_restful import Api
-from .vistas import VistaCanciones, VistaCancion
+from .modelos import db
+from .vistas import VistaCanciones, VistaCancion, VistaSignIn, VistaLogIn, VistaAlbum, VistaAlbumsUsuario, VistaCancionesAlbum
 
-app = create_app("default")
+app = create_app('default')
 app_context = app.app_context()
 app_context.push()
 
 db.init_app(app)
-db.create_all()  
+db.create_all()
 
 api = Api(app)
 api.add_resource(VistaCanciones, '/canciones')
-api.add_resource(VistaCancion, '/canciones/<int:id_cancion>')
-
-# Prueba
-with app.app_context():
-    album_schema = AlbumSchema()
-    A = Album(titulo="Album Test", anio=2023, descripcion="Test Album", medio="CD")
-    db.session.add(A)
-    db.session.commit()
-    print([album_schema.dumps(album) for album in Album.query.all()])   
+api.add_resource(VistaCancion, '/cancion/<int:id_cancion>')
+api.add_resource(VistaSignIn, '/signin')
+api.add_resource(VistaLogIn, '/login')
+api.add_resource(VistaAlbumsUsuario, '/usuario/<int:id_usuario>/albumes')
+api.add_resource(VistaAlbum, '/album/<int:id_album>')
+api.add_resource(VistaCancionesAlbum, '/album/<int:id_album>/canciones')
